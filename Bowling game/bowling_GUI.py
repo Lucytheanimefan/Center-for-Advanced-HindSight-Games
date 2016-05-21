7ac59784b6c4e3efa570e99e0d3ef0c6bb13ee89
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-05-21 09:29:37 -0700

    Added white bowling ball images

--- Bowling game/bowling_GUI.py
+++ Bowling game/bowling_GUI.py
@@ -12,7 +12,11 @@ FPS=30
 fpsClock=pygame.time.Clock()
 
 #graphics/images
+PIN_Y=50 #uppermost left pin y-coordinate
+PIN_X=20 #uppermost left pin x-coordinate
 PIN_SOLID=pygame.image.load('bowling_pin_solid.png')
+BALL_Y=550
+BALL_X=50
 
 pygame.init()
 DISPLAYSURF=pygame.display.set_mode((WIDTH,HEIGHT))
@@ -23,23 +27,29 @@ DISPLAYSURF.fill(LIGHT_BLUE)
 def displayPins(DISPLAYSURF):
 	dx=0
 	for i in range(4):
-		DISPLAYSURF.blit(PIN_SOLID,(10+dx,40))
+		DISPLAYSURF.blit(PIN_SOLID,(PIN_X+dx,PIN_Y))
 		dx=dx+160
 	dx=0
 	for j in range(3):
-		DISPLAYSURF.blit(PIN_SOLID,(100+dx,130))
+		DISPLAYSURF.blit(PIN_SOLID,(PIN_X+90+dx,PIN_Y+90))
 		dx=dx+150
 	dx=0
 	for k in range(2):
-		DISPLAYSURF.blit(PIN_SOLID,(170+dx,230))
+		DISPLAYSURF.blit(PIN_SOLID,(PIN_X+160+dx,PIN_Y+190))
 		dx=dx+140
-	DISPLAYSURF.blit(PIN_SOLID,(240,330))
+	DISPLAYSURF.blit(PIN_SOLID,(PIN_X+230,PIN_Y+290))
 
+def displayBallsLeft(DISPLAYSURF):
+	dx=0
+	for i in range(10):
+		BALL=pygame.draw.circle(DISPLAYSURF, (255,255,255), (BALL_X+dx,BALL_Y),15,0)
+		dx=dx+50
 #main game loop
 while True:
     for event in pygame.event.get():
     	#display pins to knock down
     	displayPins(DISPLAYSURF)
+    	displayBallsLeft(DISPLAYSURF)
         if event.type==QUIT:
             pygame.quit()
             sys.exit()
