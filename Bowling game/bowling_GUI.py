2bb33b03770478d10732bc2b1f1e7072e35a82b6
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-05-28 12:35:50 -0700

    Changed color of button on click

--- Bowling game/bowling_GUI.py
+++ Bowling game/bowling_GUI.py
@@ -44,6 +44,11 @@ THROWBALL=pygame.transform.scale(throwball, (300, 50))
 nextround=pygame.image.load('nextroundbutton.png')
 NEXTROUND=pygame.transform.scale(nextround, (300, 50))
 
+throwball_clicked=pygame.image.load('throwballbutton_clicked.png')
+THROWBALL_CLICKED=pygame.transform.scale(throwball_clicked, (300, 50))
+nextround_clicked=pygame.image.load('nextroundbutton_clicked.png')
+NEXTROUND_CLICKED=pygame.transform.scale(nextround_clicked, (300, 50))
+
 #initialize game
 pygame.init()
 
@@ -186,6 +191,7 @@ while True:
     	#buttons
     	mouse=pygame.mouse.get_pos()
     	if createButton(THROWBALL,0,635,300,50,mouse)==True:
+    		DISPLAYSURF.blit(THROWBALL_CLICKED,(0,635))
     		print '2nd randnums: '
     		print randnums
     		CURRENT_SCORE=CURRENT_SCORE+throwBall(BALL_COUNT, PINS, ROUNDS, randnums)
@@ -193,6 +199,7 @@ while True:
     		BALL_COUNT-=1
     	elif createButton(NEXTROUND,300,635,300,50,mouse)==True:
 			ROUNDS-=1
+			DISPLAYSURF.blit(NEXTROUND_CLICKED,(300,635))
 			#CURRENT_SCORE=CURRENT_SCORE+throwBall(BALL_COUNT,10, ROUNDS, resetPins())
 			randnums=resetPins()
 			displayScore(CURRENT_SCORE)
