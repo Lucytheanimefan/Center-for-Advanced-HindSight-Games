23ee45b0d419ac2aba3ebd65419454db148e2310
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-08-06 12:08:58 -0700

    Succesfully writing to file now

--- Snitching game/app.js
+++ Snitching game/app.js
@@ -1,11 +1,11 @@
 var file = require('file-system');
 var fs = require('fs');
-var connect = require('connect');
-var Router       = require('router');
-var finalhandler = require('finalhandler')
+//var connect = require('connect');
+//var Router       = require('router');
+//var finalhandler = require('finalhandler')
 //var Twitter = require('twitter');
-var serveStatic = require('serve-static');
-var http = require('http');
+//var serveStatic = require('serve-static');
+//var http = require('http');
 var serveIndex = require('serve-index');
 var path = require('path');
 
@@ -21,29 +21,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.use("/",serveIndex("public"));
+app.use("/", serveIndex("public"));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/sendDataToBackend', function(req,res){
-	console.log("In the backend");
-	//console.log(res);
-	//console.log(req._parsedOriginalUrl.query);
-	var tempJson = req._parsedOriginalUrl.query;
-	var json = replaceAll(tempJson, '%22','"');
-	json=replaceAll(json,"%20"," ");
-	console.log(json);
-	//console.log("Backend: "+JSON.stringify(req.body));
+app.get('/sendDataToBackend', function(req, res) {
+    console.log("In the backend");
+    //console.log(res);
+    //console.log(req._parsedOriginalUrl.query);
+    var tempJson = req._parsedOriginalUrl.query;
+    var json = replaceAll(tempJson, '%22', '"');
+    json = replaceAll(json, "%20", " ");
+    console.log(json);
+    writeDataToFile(json, false);
+    //console.log("Backend: "+JSON.stringify(req.body));
 })
 
 function replaceAll(str, find, replace) {
-  return str.replace(new RegExp(find, 'g'), replace);
+    return str.replace(new RegExp(find, 'g'), replace);
 }
 
-function writeDataToFile(data){
-	
+var stream = fs.createWriteStream("data.txt");
+
+function writeDataToFile(data) {
+    fs.appendFile('data.txt', data, function(err) {
+    	console.log(err);
+    });
 }
 
 console.log("Finished serving");
